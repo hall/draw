bd0315c7182895ab9cab081b0e7e113680091970
Bryton Hall <email@bryton.io>
2022-05-20 00:35:41 -0400

    add nix flake dev shell config

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+    inputs.utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+        };
+      });
+}
