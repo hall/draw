bff63721f4563fb53894c2ecf3c344434c7ad4c7
Bryton Hall <email@bryton.io>
2022-06-01 23:53:24 -0400

    fix save button enabled/disabled test

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,5 +1,6 @@
-import { By, Editor, EditorGroup, EditorView, VSBrowser, WebView, Workbench } from 'vscode-extension-tester';
+import { By, EditorView, VSBrowser, WebView, Workbench } from 'vscode-extension-tester';
 import { expect } from 'chai';
+import * as pkg from '../package.json';
 
 const files = {
     markdown: "test.md"
@@ -9,16 +10,16 @@ describe('Extension', () => {
     /** helper to wait X milliseconds */
     const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
     let webview: WebView;
+    let workbench: Workbench;
 
     before('open webview', async function () {
         this.timeout(120000); // ci needs some time
 
-        await VSBrowser.instance.openResources(files.markdown);
-
-        await new Workbench().executeCommand('draw edit');
+        workbench = await new Workbench();
+        await workbench.executeCommand('draw edit');
         await sleep(1000);
 
-        webview = await new EditorView().openEditor('Draw', 1) as WebView;
+        webview = await new EditorView().openEditor(pkg.displayName, 1) as WebView;
     });
 
     it('is displayed', async function () {
@@ -36,15 +37,17 @@ describe('Extension', () => {
             button = await webview.findWebElement(By.id("svg-save"));
         });
 
-        it('is enabled when an editor exists', async function () {
-            expect(await button.isEnabled()).to.be.true;
+        it('is disabled when no active editor exists', async function () {
+            expect(button.isEnabled()).not.to.be.true;
         });
 
-        // it('is disabled when no active editor exists', async function () {
-        //     editor.closeEditor(files.markdown);
-
-        //     expect(await button.isEnabled()).not.to.be.true;
-        // });
+        it('is enabled when an editor exists', async function () {
+            await webview.switchBack();
+            await workbench.executeCommand('focus left group');
+            await VSBrowser.instance.openResources(files.markdown);
+            await webview.switchToFrame();
+            expect(await button.isEnabled()).to.be.true;
+        });
 
     });
 
